7d25bcabaa027ebccca889981323ea1234e7b084
Yuyang Peng <yuyang.peng@tu-dortmund.de>
2022-05-17 17:31:10 +0200

    update

--- src/main/java/com/example/KaffeeMain.java
+++ src/main/java/com/example/KaffeeMain.java
@@ -1,29 +1,28 @@
 package com.example;
-
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.*;
 
+
 public class KaffeeMain extends AbstractBehavior<KaffeeMain.StartMessage> {
-
     public static class StartMessage {}
-    // lskdjfkdjgsdkfdfgdf
-
     ActorRef<SomeActor.SomeMessage> someActor;
-
     public static Behavior<StartMessage> create() {
         return Behaviors.setup(KaffeeMain::new);
     }
+
 
     private KaffeeMain(ActorContext<StartMessage> context) {
         super(context);
         someActor = context.spawn(SomeActor.create(37), "someActor");
     }
 
+
     @Override
     public Receive<StartMessage> createReceive() {
         return newReceiveBuilder().onMessage(StartMessage.class, this::onStartMessage).build();
     }
+
 
     private Behavior<StartMessage> onStartMessage(StartMessage command) {
         someActor.tell(new SomeActor.SomeMessage());
