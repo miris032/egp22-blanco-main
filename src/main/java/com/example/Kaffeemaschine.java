dd0505d330b2938a2a281c6c7434a525597154bf
lizihao <li.zihao.1998.12.12@outlook.com>
2022-05-18 21:51:02 +0200

    不知道是不是这个意思嗷

--- src/main/java/com/example/Kaffeemaschine.java
+++ src/main/java/com/example/Kaffeemaschine.java
@@ -1,38 +1,53 @@
 package com.example;
+import akka.actor.ActorRef;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
+import uebung04.Lagerist;
+import uebung04.Lagerverwaltung;
 
 
 public class Kaffeemaschine extends AbstractBehavior<Kaffeemaschine.SomeMessage> {
 
     private int Vorrat;
+    public interface Request {}
+    public static final class make implements Kaffeemaschine.Request {
+        public final ActorRef<Loadbalancer.Response> sender;
+        public make(ActorRef<Loadbalancer.Response> sender) {
+            this.sender = sender;
+        }
+    }
 
-    private final int someAttribute;
-    public static final class SomeMessage {}
-
-
-    public static Behavior<SomeMessage> create(int someAttribute) {
-        return Behaviors.setup(context -> new Kaffeemaschine(context, someAttribute));
+    public static Behavior<Kaffeemaschine.Request> create(int Vorrat) {
+        return Behaviors.setup(context -> new Kaffeemaschine(context, Vorrat));
     }
 
 
-    private Kaffeemaschine(ActorContext<SomeMessage> context, int someAttribute) {
+    //Constructor
+    private Kaffeemaschine(ActorContext<Kaffeemaschine.Request> context, int Vorrat) {
         super(context);
-        this.someAttribute = someAttribute;
+        this.Vorrat = Vorrat;
     }
 
 
     @Override
-    public Receive<SomeMessage> createReceive() {
-        return newReceiveBuilder().onMessage(SomeMessage.class, this::onSomeMessage).build();
+    public Receive<Kaffeemaschine.Request> createReceive() {
+        return newReceiveBuilder()
+                .onMessage(make.class, this::oneMake)
+                .build();
     }
 
 
-    private Behavior<SomeMessage> onSomeMessage(SomeMessage command) {
-        getContext().getLog().info("Got a message. My attribute is {}!", someAttribute);
+    private Behavior<Kaffeemaschine.Request> oneMake(Kaffeemaschine.make request) {
+        getContext().getLog().info("Got a get request from {} ({})!", request.sender.path(), Vorrat);
+        if (this.Vorrat > 0) {
+            this.Vorrat -= 1;
+            request.sender.tell(new Loadbalancer.Success());
+        } else {
+            request.sender.tell(new Loadbalancer.Fail());
+        }
         return this;
     }
 }
